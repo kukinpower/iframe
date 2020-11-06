4206416d8f8d1dede678640846d8a404f82af32c
Messenger Triston <mtriston@am-k1.msk.21-school.ru>
2020-11-06 20:08:55 +0300

    make red

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function change() {
 
     let frm = parent.document.getElementById('frm');
-    alert(frm);
-    // let title = frame;
-    // title[0].style.color = "red";
+    frm.style.backgroundColor = "red";
+
 }
