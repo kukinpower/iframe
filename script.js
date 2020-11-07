7aaadf6489eb5ffcc0a832771d651d5c9fe50512
Il'q Sagajdak <lhelper@am-j1.msk.21-school.ru>
2020-11-07 21:16:28 +0300

    before merge

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ let bg1 = ["#2c3a47", "#2c2c47", "#472c42", "#2c3f47", "#2c473e", "#472c2c", "me
 let bg2 = ["grey", "mediumpurple", "#472c2c", "#2c473e", "#2c3f47", "#472c42", "#2c2c47", "#2c3a47"];
 let i = 0;
 
+let frm = parent.document.getElementById('frm');//.contentWindow;
+let frame1 = parent.document.getElementById('frame1');//.contentWindow;
+//frm.style.backgroundColor = bg1[i%bg1.length];
+//frame1.style.backgroundColor = bg2[i%bg2.length];
+
 function change() {
-    let frm = parent.document.getElementById('frm');//.contentWindow;
-    let frame1 = parent.document.getElementById('frame1');//.contentWindow;
+    i++;
     frm.style.backgroundColor = bg1[i%bg1.length];
     frame1.style.backgroundColor = bg2[i%bg2.length];
-    i++;
 }
 
 function changeOne() {
-    let frm = parent.document.getElementById('frm');
-    let frame1 = parent.document.getElementById('frame1');//.contentWindow;
     i++;
     frame1.style.backgroundColor = bg2[i%bg2.length];
     if (frame1.style.backgroundColor == frm.style.backgroundColor)
     {
         console.log("HELL");
-        let f1 = parent.document.getElementById('frm');
-        let f2 = parent.document.getElementById('frame1');
-        let f11 = f1.contentWindow.document.getElementById("#1");
-        let f22 = f2.contentWindow.document.getElementById("#2");
-        f11.style.display = "block";
-        f22.style.display = "block";
+        let f1 = frm.contentWindow.document.getElementById("second");
+        console.log(f1);
+        let f2 = frame1.contentWindow.document.getElementById("first");
+        console.log(f2);
+        f1.style.display = "block";
+        f2.style.display = "block";
     }
 }
